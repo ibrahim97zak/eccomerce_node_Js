c5d7c150fa2b21fab5f121e5370eca313ed5fdeb
ibrahim97zak <121764716+ibrahim97zak@users.noreply.github.com>
2024-02-14 15:42:08 +0200

    mong-connect-newparse

--- DB/connection.js
+++ DB/connection.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
-
 const connectDB = async ()=>{
 
-    return await mongoose.connect(process.env.DB_LOCAL)
+    return await mongoose.connect(process.env.DB_LOCAL,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then( ()=>{
         console.log("connect db");
     } ).catch( (err)=>{
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import initApp from './src/Modules/app.router.js';
 // import createInvoice from './src/Services/pdf.js';
 
 const app = express();
+
 const PORT =process.env.PORT || 3000;
 app.set(`case sensitive routing`,true)
 initApp(app,express);
