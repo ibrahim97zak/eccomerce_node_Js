a4c5ef856a5bb538ff7ed2b1c56ce18043b65ce1
ibrahim97zak <121764716+ibrahim97zak@users.noreply.github.com>
2024-02-14 15:50:50 +0200

    catch-error-fix

--- src/Services/errorHandling.js
+++ src/Services/errorHandling.js
@@ -10,6 +10,6 @@ export const globalErrorHandel = (err,req,res,next)=>{
     if(process.env.MOOD=='DEV'){
         return res.status(err.cause || 500).json({message:'catch error',stack:err.stack});
     }else{
-        return res.status(err.cause || 500).json({message:'catch error'})
+        return res.status(err.cause || 500).json({message:'catch error',stack:err.stack})
     }
 }
